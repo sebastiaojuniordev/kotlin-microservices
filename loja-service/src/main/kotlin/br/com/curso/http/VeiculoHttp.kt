2967e19b1c6ee4968b2f1e7052c3600c543632f2
Sebastião Junior <77406058+sebastiaojuniordev@users.noreply.github.com>
2021-11-22 19:58:49 -0300

    project loja-service

---
+++ loja-service/src/main/kotlin/br/com/curso/http/VeiculoHttp.kt
@@ -0,0 +1,16 @@
+package br.com.curso.http
+
+import br.com.curso.dto.output.Veiculo
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.retry.annotation.CircuitBreaker
+
+@Client(id = "http://localhost:8080")
+@CircuitBreaker
+interface VeiculoHttp {
+
+    @Get("/veiculos/{id}")
+    fun findById(@PathVariable id: Long): Veiculo
+}
---
+++ loja-service/src/main/kotlin/br/com/curso/service/VendaService.kt
@@ -0,0 +1,31 @@
+package br.com.curso.service
+
+import br.com.curso.dto.input.VendaInput
+import br.com.curso.dto.output.Parcela
+import br.com.curso.dto.output.Venda
+import br.com.curso.http.VeiculoHttp
+import jakarta.inject.Singleton
+import java.time.LocalDate
+
+@Singleton
+class VendaService(
+    private val veiculoHttp: VeiculoHttp
+) {
+
+    fun realizarVenda(vendaInput: VendaInput) {
+        val veiculo = veiculoHttp.findById(vendaInput.veiculo)
+        var parcelas: List<Parcela> = ArrayList<Parcela>()
+        var valorParcela = vendaInput.valor.divide(vendaInput.quantidadeParcelas.toBigDecimal())
+        var dataVencimento = LocalDate.now().plusMonths(1);
+
+        for (i in 1..vendaInput.quantidadeParcelas) {
+            var parcela = Parcela(valorParcela, dataVencimento.toString())
+            parcelas = parcelas.plus(parcela)
+            dataVencimento = dataVencimento.plusMonths(1);
+        }
+
+        var venda = Venda(vendaInput.cliente, veiculo, vendaInput.valor, parcelas)
+
+        println(venda)
+    }
+}
